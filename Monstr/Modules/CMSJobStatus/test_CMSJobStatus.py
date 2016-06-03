c42f0afe025fc28b6b3e04622e6ea121aec99913
IgorPelevanyuk <gavelock@gmail.com>
2016-06-03 12:04:31 +0000

    TEST-UPDATE: try to use mocks

--- Monstr/Modules/CMSJobStatus/test_CMSJobStatus.py
+++ Monstr/Modules/CMSJobStatus/test_CMSJobStatus.py
@@ -2,12 +2,15 @@ from Monstr.Core import Runner
 import mock 
 
 
-def test_CMSJobStats_withRunner():
+def test_CMSJobStatus_withRunner():
     import datetime
+    import Monstr.Modules.CMSJobStatus
+    
     modules = Runner.get_modules()
     assert 'CMSJobStatus' in modules
     if 'CMSJobStatus' in modules:
-        with mock.patch("modules['CMSJobStatus'].Utils.get_UTC_now") as MockClass:
-	    MockClass.return_value = datetime.datetime.utcnow().replace(tzinfo=pytz.utc) - datetime.timedelta(hours=1)
+        module = modules['CMSJobStatus']
+        with mock.patch("module.Utils.get_UTC_now") as MockClass:
+            MockClass.return_value = datetime.datetime.utcnow().replace(tzinfo=pytz.utc) - datetime.timedelta(hours=1)
             modules['CMSJobStatus'].InsertToDB()
 
