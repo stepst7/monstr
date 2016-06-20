1a81640bff2e86417c1e356bbfc090e1e0ad1094
IgorPelevanyuk <gavelock@gmail.com>
2016-06-20 16:59:34 +0300

    TEST-FIXED: Wrapped in "def test_...():"

--- Monstr/Modules/SSB/test_SSB.py
+++ Monstr/Modules/SSB/test_SSB.py
@@ -1,6 +1,13 @@
 from Monstr.Core import Runner
 
-modules = Runner.get_modules()
-assert 'SSB' in modules
-if 'SSB' in modules:
-     modules['SSB'].InsertToDB()
+def test_SSB_initial():
+    modules = Runner.get_modules()
+    assert 'SSB' in modules
+    if 'SSB' in modules:
+         modules['SSB'].InsertToDB()
+
+def test_SSB_additional():
+    modules = Runner.get_modules()
+    assert 'SSB' in modules
+    if 'SSB' in modules:
+         modules['SSB'].InsertToDB()
