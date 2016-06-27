357af11f5745fa6ddb615955e652bee8d103fbd7
IgorPelevanyuk <gavelock@gmail.com>
2016-06-27 18:05:42 +0300

    UPDATE: DB drop and create table if fields changed

--- Monstr/Core/DB.py
+++ Monstr/Core/DB.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Table, Column, Integer, String, DateTime, Enum, Text, UniqueConstraint, MetaData
+from sqlalchemy import Table, Column, Integer, String, BigInteger, DateTime, Enum, Text, UniqueConstraint, MetaData
 from sqlalchemy import create_engine
 from sqlalchemy.exc import NoSuchTableError
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import inspect
 
 import Monstr.Core.Config as Config
 
@@ -20,6 +21,9 @@ class DBHandler():
     session = None
 
     def __init__(self):
+        self.initDB()
+
+    def initDB(self):
         db_conf = Config.get_section('Database')
         user, password, host, name = db_conf['user'], db_conf['password'], db_conf['host'], db_conf['name']
         connection_string = 'postgresql://' + user + ':' + password + '@' + host + '/' + name
@@ -29,19 +33,26 @@ class DBHandler():
         self.metadata = MetaData(self.engine)
 
     def makeTable(self, name, schema):
-        table = Table(name, self.metadata, *schema)
+        table = Table(name, self.metadata, *schema, extend_existing=True)
         table.create(checkfirst=True)
         return table
 
     def getOrCreateTable(self, name, schema):
-        metadata = self.metadata
-        table = None
-        try:
-            table = Table(name, metadata, autoload=True)
-            print 'Table exist'
-        except NoSuchTableError:
+        iengine = inspect(self.engine)
+        db_tables = iengine.get_table_names()
+
+        if name not in db_tables:
             self.makeTable(name, schema)
-            table = Table(name, metadata, autoload=True)
+        table = Table(name, self.metadata, autoload=True)
+        db_columns = iengine.get_columns(name)
+        db_column_names = [c["name"] for c in iengine.get_columns(name)]
+        model_column_names = [c.name for c in schema if c.name!=None]
+        if set(model_column_names) != set(db_column_names):
+            print 'do not correspond'
+            self.session.close()
+            table.drop()
+            table = self.makeTable(name, schema)
+
         return table
 
     def initialize(self, schemas, prefix):
