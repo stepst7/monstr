28bf56a36f390fa8c1a32de023fb503ca8c150df
IgorPelevanyuk <gavelock@gmail.com>
2016-07-29 11:52:29 +0300

    UPDATE: Journal added to BaseModule. Result of all tests are written in Journal now.

--- Monstr/Core/BaseModule.py
+++ Monstr/Core/BaseModule.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta, abstractmethod
 import Monstr.Core.DB as DB
+import Monstr.Core.Utils as Utils
 
 
 
@@ -27,6 +28,23 @@ class BaseModule():
     tables = None
     db_handler = None
 
+    journal_schema = (DB.Column('id', DB.Integer, primary_key=True),
+                      DB.Column('module', DB.String(64)),
+                      DB.Column('time', DB.DateTime(True)),
+                      DB.Column('result', DB.String(32)),
+                      DB.Column('step', DB.String(32)),
+                      DB.Column('description', DB.Text),)
+
+    def _create_journal_row(self, result, step=None, error=None):
+        row = {'module': self.name,
+               'time': Utils.get_UTC_now(),
+               'result': result,
+               'step': step,
+               'description': (type(error).__name__ + ': ' + error.message) if error is not None else None
+        }
+        return row
+
+
     rest_links = {}
 
     def __init__(self):
@@ -53,7 +71,35 @@ class BaseModule():
             self.db_handler.bulk_insert(table, data[schema])        
 
     def ExecuteCheck(self):
-        self.Initialize()
-        params = self.PrepareRetrieve()
-        data = self.Retrieve(params)
-        self.InsertToDB(data)
+        journal = self.db_handler.getOrCreateTable('monstr_Journal', self.journal_schema)
+        try:
+            self.Initialize()
+        except Exception as e:
+            row = self._create_journal_row('Fail', 'Initialize', e)
+            self.db_handler.insert(journal, row)
+            return
+
+        try:
+            params = self.PrepareRetrieve()
+        except Exception as e:
+            row = self._create_journal_row('Fail', 'PrepareRetrieve', e)
+            self.db_handler.insert(journal, row)
+            return
+
+        try:
+            data = self.Retrieve(params)
+        except Exception as e:
+            row = self._create_journal_row('Fail', 'Retrieve', e)
+            self.db_handler.insert(journal, row)
+            return
+
+        try:
+            self.InsertToDB(data)
+        except Exception as e:
+            row = self._create_journal_row('Fail', 'InsertToDB', e)
+            self.db_handler.insert(journal, row)
+            return
+
+        row = self._create_journal_row('Success')
+        self.db_handler.insert(journal, row)
+        
